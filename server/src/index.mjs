43621ce940968d70470957b645f6fe3dce37a91b
Eileen Burdekin <eileenburdekin@proton.me>
2024-10-16 19:44:05 -0700

    add middleware

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,39 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
+// import authRoutes from "./routes/auth";
+// import userRoutes from "./routes/users";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Supabase Client Setup
+// Supabase client setup
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
 
-// Middleware
+// middleware
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    // only accept requests from defined URL, and cookie must be included in request
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
-// Basic route to test Supabase connection
+// index
 app.get("/", async (req, res) => {
-  try {
-    const { data, error } = await supabase
-      .from("users") // Replace with your actual table name
-      .select("*");
-
-    if (error) {
-      throw error;
-    }
-
-    res.status(200).json(data);
-  } catch (error) {
-    console.error("Error fetching data from Supabase:", error);
-    res.status(500).json({ message: "Error fetching data" });
-  }
+  res.status(200).json("gut-feelings API");
 });
 
+// register endpoint with Express
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+
+// start Express server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
