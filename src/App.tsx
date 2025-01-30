48396d6d2637490cf338b622c96be3832c7042e0
Eileen Burdekin <eileenburdekin@proton.me>
2025-01-29 21:19:11 -0800

    add mantine packages, postcss.config.cjs, MantineProvider wrapper in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
+// src/App.tsx
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AppContextProvider } from "./contexts/AppContext";
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Test from "./pages/Test";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AuthRedirect from "./pages/AuthRedirect";
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Test />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/auth/redirect" element={<AuthRedirect />} />
-        <Route path="/dashboard" element={<p>Dashboard</p>} />
-        <Route />
-      </Routes>
-    </Router>
-  );
-};
-export default App;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // if you get an error, don't auto-retry (react-query default setting)
+      retry: 0,
+    },
+  },
+});
+
+const theme = createTheme({
+  // fontFamily: "Montserrat, sans-serif",
+  // defaultRadius: "md",
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppContextProvider>
+        <MantineProvider theme={theme}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Test />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<SignIn />} />
+              <Route path="/auth/redirect" element={<AuthRedirect />} />
+              <Route path="/dashboard" element={<p>Dashboard</p>} />
+              <Route />
+            </Routes>
+          </Router>
+        </MantineProvider>
+      </AppContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
